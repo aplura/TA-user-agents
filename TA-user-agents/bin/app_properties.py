8d0c56858022e81a42f5f93d340223aa11db9040
Aplura CI/CD <customersupport@aplura.com>
2026-03-13 13:47:07 +0000

    Fri Mar 13 09:33:58 EDT 2026 PTUA-9: Update to python 3.13, Overview dashboard

--- TA-user-agents/bin/app_properties.py
+++ TA-user-agents/bin/app_properties.py
@@ -1,3 +1,3 @@
 __app_name__ = "TA-user-agents"
 __version__ = "1.7.11"
-__build__ = "52"
+__build__ = "53"
--- TA-user-agents/bin/version.py
+++ TA-user-agents/bin/version.py
@@ -1,1 +1,1 @@
-__version__='TA-user-agents.1.7.11b52'
+__version__='TA-user-agents.1.7.11b53'
